4c76160010e500c8d0950de30b082566ba935399
oleg <fedorovo727@gmail.com>
2026-09-07 15:44:05 +0300

    [TASK-3] implement solution

---
+++ fedorov.oleg/lab3/solution.js
@@ -0,0 +1,13 @@
+export function shuffleArray(arr) {
+  if (!Array.isArray(arr)) {
+    return [];
+  }
+
+  const newArr = [...arr];
+  for (let i = newArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+  }
+  
+  return newArr;
+}
